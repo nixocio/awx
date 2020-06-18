994b9fec801fcac07c711f7ed9ab211e0783c564
nixocio <nixocio@gmail.com>
2020-06-18 10:12:08 -0400

    Add feature to add Credential Type
    Add feature to add Credential Type.
    
    See: https://github.com/ansible/awx/issues/7325

--- awx/ui_next/src/screens/CredentialType/CredentialTypeAdd/CredentialTypeAdd.jsx
+++ awx/ui_next/src/screens/CredentialType/CredentialTypeAdd/CredentialTypeAdd.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, PageSection } from '@patternfly/react-core';
+import { useHistory } from 'react-router-dom';
+
+import CredentialTypeForm from '../shared/CredentialTypeForm';
+import { CardBody } from '../../../components/Card';
+import { CredentialTypesAPI } from '../../../api';
+import { parseVariableFields } from '../../../util/yaml';
 
 function CredentialTypeAdd() {
+  const history = useHistory();
+  const [submitError, setSubmitError] = useState(null);
+
+  const handleSubmit = async values => {
+    try {
+      const { data: response } = await CredentialTypesAPI.create({
+        ...values,
+        injectors: parseVariableFields(values.injectors),
+        inputs: parseVariableFields(values.inputs),
+        kind: 'cloud',
+      });
+      history.push(`/credential_types/${response.id}/details`);
+    } catch (error) {
+      setSubmitError(error);
+    }
+  };
+
+  const handleCancel = () => {
+    history.push(`/credential_types`);
+  };
+
   return (
     <PageSection>
       <Card>
-        <div>Credentials Type Add</div>
+        <CardBody>
+          <CredentialTypeForm
+            onSubmit={handleSubmit}
+            submitError={submitError}
+            onCancel={handleCancel}
+          />
+        </CardBody>
       </Card>
     </PageSection>
   );
