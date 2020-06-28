d79cbe6538e16372219fa7343a4c656335b33c4a
nixocio <nixocio@gmail.com>
2020-06-28 15:02:42 -0400

    WIP - Add Credential Type Edit

--- awx/ui_next/src/screens/CredentialType/CredentialTypeEdit/CredentialTypeEdit.jsx
+++ awx/ui_next/src/screens/CredentialType/CredentialTypeEdit/CredentialTypeEdit.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { Card, PageSection } from '@patternfly/react-core';
+import { CardBody } from '../../../components/Card';
 
-function CredentialTypeEdit() {
+import { CredentialTypesAPI } from '../../../api';
+import CredentialTypeForm from '../shared/CredentialTypeForm';
+
+function CredentialTypeEdit(credentialType) {
+  const history = useHistory();
+  const { id } = useParams();
+  const [submitError, setSubmitError] = useState(null);
+
+  const handleSubmit = async (...values) => {
+    console.log({...values})
+    try {
+      await CredentialTypesAPI.update(id, values);
+      history.push(`/credential_types/${id}/details`);  
+    } catch (error) {
+      setSubmitError(error);
+    }
+  };
+
+  const handleCancel = () => {
+    history.push(`/credential_types/${id}/details`);
+  };
+
   return (
     <PageSection>
-      <Card>Credential Type Edit</Card>
+      <Card>
+        <CardBody>
+          <CredentialTypeForm
+            credentialType={credentialType}
+            onSubmit={handleSubmit}
+            submitError={submitError}
+            onCancel={handleCancel}
+          />
+        </CardBody>
+      </Card>
     </PageSection>
   );
 }
