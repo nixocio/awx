71a4ff9441dbc9ef33bb43bca9ff7f91f44fca87
nixocio <nixocio@gmail.com>
2021-03-15 16:09:39 -0400

    Do not allow user to modify EE managed by tower
    Do not allow user to attempt to modify EE managed by tower.
    
    See: https://github.com/ansible/awx/issues/9250

--- awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentList/ExecutionEnvironmentListItem.test.jsx
+++ awx/ui_next/src/screens/ExecutionEnvironment/ExecutionEnvironmentList/ExecutionEnvironmentListItem.test.jsx
@@ -14,6 +14,7 @@ describe('<ExecutionEnvironmentListItem/>', () => {
     organization: null,
     credential: null,
     summary_fields: { user_capabilities: { edit: true } },
+    managed_by_tower: false,
   };
 
   test('should mount successfully', async () => {
@@ -70,5 +71,47 @@ describe('<ExecutionEnvironmentListItem/>', () => {
     ).toBe('Globally Available');
 
     expect(wrapper.find('PencilAltIcon').exists()).toBeTruthy();
+  });
+
+  test('should not render the pencil action for ee managed by tower', async () => {
+    await act(async () => {
+      wrapper = mountWithContexts(
+        <table>
+          <tbody>
+            <ExecutionEnvironmentListItem
+              executionEnvironment={{
+                ...executionEnvironment,
+                summary_fields: { user_capabilities: { edit: false } },
+                managed_by_tower: true,
+              }}
+              detailUrl="execution_environments/1/details"
+              isSelected={false}
+              onSelect={() => {}}
+            />
+          </tbody>
+        </table>
+      );
+    });
+    expect(
+      wrapper
+        .find('Td')
+        .at(1)
+        .text()
+    ).toBe(executionEnvironment.name);
+    expect(
+      wrapper
+        .find('Td')
+        .at(2)
+        .text()
+    ).toBe(executionEnvironment.image);
+
+    expect(
+      wrapper
+        .find('Td')
+        .at(3)
+        .text()
+    ).toBe('Globally Available');
+
+    expect(wrapper.find('PencilAltIcon').exists()).toBeFalsy();
   });
 });
